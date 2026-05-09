f0ed6ed2da7344ba4a8e98c51f0c5e6eb3a13a70
Susana777 <susananolascoalvarez97@gmail.com>
2026-05-09 13:27:05 -0600

    Add files via upload

---
+++ SDTP/MUX2a1_5b.v
@@ -0,0 +1,15 @@
+module mux2a1_5b(
+    input  [31:0] entrada1, entrada2,
+    input  selector,
+    output reg [4:0] Resultado
+);
+
+always @(*) begin
+    case (selector)
+        1'b0: Resultado = entrada1;
+        1'b1: Resultado = entrada2;
+        default: Resultado = 32'b0;
+    endcase
+end
+
+endmodule
